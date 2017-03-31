ccaead4e1df0cd86f6a7d0209a33cf0ca4872bd3
Michael Dustin Riley Riley <mdrdatalab@gmail.com>
2017-03-31 17:55:30 -0400

    better slides

--- res/js/platSlides.js
+++ res/js/platSlides.js
@@ -1,20 +1,14 @@
-var platform = [
-"<b>1.0 PERSONAL LIBERTY</b><br>
-Individuals should be free to make choices for themselves and must accept responsibility for the consequences of the choices they make. Our support of an individual’s right to make choices in life does not mean that we necessarily approve or disapprove of those choices. No individual, group, or government may initiate force against any other individual, group, or government.",
-"<b>1.1 Self-Ownership</b>
-<br>
-Individuals own their bodies and have rights over them that other individuals, groups, and governments may not violate. Individuals have the freedom and responsibility to decide what they knowingly and voluntarily consume, and what risks they accept to their own health, finances, safety, or life."
-
-]
-
 var slideIndex = 0
 carousel();
 
 function carousel(){
-
-	document.getElementById('plat').innerHTML = platform[i]
-	slideIndex++
-	if slidIndex > platform.length){slideINdex = 1}
-	setTimeout(carousel, 5000)
-
+	var i;
+	var x = document.getElementsByClassName("slides");
+	for (i = 0; i < x.length; i++){
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {slideIndex = 1}
+	x[slideIndex-1].style.display = "block"
+	setTimeout(carousel, 3000)
 }
